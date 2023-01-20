21b562dd7120a283958f918ad062800a2903b6d6
mauriciomonta <mauriciomontadg@gmail.com>
2023-01-20 11:04:40 -0300

    new font & changes

--- src/components/Working.js
+++ src/components/Working.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export default function Working() {
-  return (
-    <div className='fullcontainer'>
-        <div className='workingcontent'>
-            <h1>WE'RE WORKING ON...</h1>
-            <img src='https://imgur.com/iI5J6Jx.png' alt='working on it' width={400}/>
-        </div>
-    </div>
-  )
+	return (
+		<div className="fullcontainer">
+			<div className="workingcontent">
+				<h1>We're workin'</h1>
+				<img src="https://imgur.com/B3lOc3G.png" alt="working on it" width={400} />
+			</div>
+		</div>
+	);
 }
