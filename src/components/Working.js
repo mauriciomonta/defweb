3c837793ba0fc23bd1aa8f5ae7790e24f437fc73
mauriciomonta <mauriciomontadg@gmail.com>
2023-01-20 10:28:34 -0300

    first commit

---
+++ src/components/Working.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Working() {
+  return (
+    <div className='fullcontainer'>
+        <div className='workingcontent'>
+            <h1>WE'RE WORKING ON...</h1>
+            <img src='https://imgur.com/iI5J6Jx.png' alt='working on it' width={400}/>
+        </div>
+    </div>
+  )
+}
